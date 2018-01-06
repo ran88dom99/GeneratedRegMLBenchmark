13ea6d2e24bfc186d9b2a6deee55c3ec47112d08
Ma Shin <vacation57connect@gmail.com>
2018-01-05 20:06:38 -0500

    auto

--- reset.sh
+++ reset.sh
@@ -3,10 +3,11 @@
 killall rsession
 killall Rscript
 killall R
- cd C:\Users\gvg\Desktop\Gen Test\gentest
+
 git add -A
 git commit -a -m "auto"
 git pull
 git mergetool
 git push origin
 Rscript 'Model Tester Quick .R'
+ cd C:\Users\gvg\Desktop\Gen Test\gentest
