532feb9eaed306d4b09ce9fff4c6d70625885f00
Ma Shin <vacation57connect@gmail.com>
2018-01-11 18:46:46 -0500

    auto

--- rungrandpredictorhp.bat
+++ rungrandpredictorhp.bat
@@ -3,4 +3,4 @@ cd "C:\Users\gvg\Desktop\Gen Test\gentest\"
 
 bunchofgits.sh
 
-"C:\Users\gvg\Documents\R\R-3.4.1\bin\x64\R.exe" CMD BATCH ModelTesterAllAuto.R
+"C:\Users\gvg\Documents\R\R-3.4.3\bin\x64\R.exe" CMD BATCH ModelTesterAllAuto.R
