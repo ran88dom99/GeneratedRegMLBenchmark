8b8df2439028b810e28051f6bebefb6864a283b4
Ma Shin <vacation57connect@gmail.com>
2018-02-16 17:07:46 -0500

    auto

--- bunchofgitstheirsauto.sh
+++
@@ -1,7 +0,0 @@
- #!/bin/bash
-
-git add -A
-git commit -a -m "auto"
-git pull --no-edit -s recursive -X theirs
-
-git push origin
---
+++ rungrandpredictorlapir.bat
@@ -0,0 +1,8 @@
+taskkill /f /im java.exe
+taskkill /f /im Rterm.exe
+
+cd "C:\Users\irina grishina\Desktop\generated data test"
+
+bunchofgitstheirsauto.sh
+
+"C:\Program Files\R\R-3.4.1\bin\x64\R.exe" CMD BATCH ModelTesterAllAuto.R
