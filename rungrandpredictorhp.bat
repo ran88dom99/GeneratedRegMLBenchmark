aa8db95b2a7ed56013c3882bb115b22f467e46dd
Ma Shin <vacation57connect@gmail.com>
2018-03-12 16:25:17 -0400

    auto

--- rungrandpredictorhp.bat
+++ rungrandpredictorhp.bat
@@ -2,6 +2,10 @@ taskkill /f /im java.exe
 taskkill /f /im Rterm.exe
 
 cd "C:\Users\gvg\Desktop\Gen Test\gentest\"
+set isodate=%date:~10,4%-%date:~4,2%-%date:~7,2%
+ren ModelTesterAllAuto.Rout rout%isodate%.txt
+move rout%isodate%.txt "C:\Users\gvg\Desktop\Gen Test\gentest\HOPPER"
+cd "C:\Users\gvg\Desktop\Gen Test\gentest\"
 
 bunchofgitstheirsauto.sh
 
