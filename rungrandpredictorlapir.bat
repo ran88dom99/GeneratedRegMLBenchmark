82dba921f078247ec8be2ce1dc84949768491a02
ran88dom99 <rain8dome9@gmail.com>
2018-03-12 16:48:43 -0400

    auto

--- rungrandpredictorlapir.bat
+++ rungrandpredictorlapir.bat
@@ -1,8 +1,13 @@
+
 taskkill /f /im java.exe
 taskkill /f /im Rterm.exe
 
 cd "C:\Users\irina grishina\Desktop\generated data test"
+ren ModelTesterAllAuto.Rout rout.txt
+move rout.txt "C:\Users\John\Documents\GitHub\GeneratedRegMLBenchmark\ACEREBOUT"
+cd "C:\Users\John\Documents\GitHub\GeneratedRegMLBenchmark"
 
 bunchofgitstheirsauto.sh
 
-"C:\Program Files\R\R-3.4.1\bin\x64\R.exe" CMD BATCH ModelTesterAllAuto.R
+"C:\Program Files\R\R-3.4.1\bin\x64\R.exe" CMD BATCH --max-mem-size=2000 ModelTesterAllAuto.R
+
