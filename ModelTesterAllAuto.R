43ff316aad39b1ca812de46a420807eb66aeb01d
Ma Shin <vacation57connect@gmail.com>
2018-06-23 22:47:45 -0400

    auto

--- ModelTesterAllAuto.R
+++ ModelTesterAllAuto.R
@@ -16,7 +16,7 @@ which.computer<-Sys.info()[['nodename']]
 task.subject<-"outg15th10hp10cv"#"carEnstest3"#
 if(which.computer=="ALTA") {task.subject<-"carEnstest3"}
 pc.mlr<-c("ACEREBOUT","HOPPERt","ALTAa")#T,"HOPPER"
-pc.carems<-c("HOPPER","ALTA","ACEREBOUTf")
+pc.carems<-c("HOPPERt","ALTA","ACEREBOUTf")
 if(which.computer=="ACEREBOUT") task.subject<-"hffoldrecc20hp20cv20hf"
 out.file<-paste("out",task.subject,which.computer,.Platform$OS.type,.Platform$r_arch,".csv",sep="")
 importance.file<-paste("importance",task.subject,which.computer,.Platform$OS.type,.Platform$r_arch,sep="")
