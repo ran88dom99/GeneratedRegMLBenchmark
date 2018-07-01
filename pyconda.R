b891478be2540892772d5622df740af92c704c8d
ran88dom99 <rain8dome9@gmail.com>
2018-07-01 13:10:09 -0400

    auto

--- pyconda.R
+++ pyconda.R
@@ -1,6 +1,3 @@
-#http://proceedings.mlr.press/v64/olson_tpot_2016.pdf
-library(reticulate)
-
 # Load a dataset from the MASS package.
 data(Boston, package = "MASS")
 
@@ -23,38 +20,55 @@ outcome = Boston$medv
 
 # Create a dataframe to contain our explanatory variables.
 data = subset(Boston, select = -medv)
+# Reduce to a dataset of 150 observations to speed up model fitting.
+train_obs = sample(nrow(data), 400)
 
+# X is our training sample.
+X_train = data[train_obs, ]
+
+# Create a holdout set for evaluating model performance.
+# Note: cross-validation is even better than a single holdout sample.
+X_holdout = data[-train_obs, ]
+
+# Create a binary outcome variable: towns in which median home value is > 22,000.
+
+Y_train = outcome[train_obs]
+Y_holdout = outcome[-train_obs]
+
+#http://proceedings.mlr.press/v64/olson_tpot_2016.pdf
+library(reticulate)
 
 when <- proc.time()
 
 
-
 if(T){
-  generationcount = r_to_py(3)
-  retainpopulation = r_to_py(50)
-  offspring_size = r_to_py(100)
-  cv = r_to_py(10)
-  random_state = r_to_py(222)
-  early_stop = r_to_py(3)
-  mins_onapipe = r_to_py(15)
+  generationcount = r_to_py(as.integer(3))
+  retainpopulation = r_to_py(as.integer(50))
+  offspring_size = r_to_py(as.integer(100))
+  cv = r_to_py(as.integer(10))
+  random_state = r_to_py(as.integer(222))
+  early_stop = r_to_py(as.integer(3))
+  mins_onapipe = r_to_py(as.integer(15))
   checkpoint_folder = r_to_py("tpot")
   pipefile = r_to_py('tpot_boston_pipeline.py')
 }
-
-
 
 tpot <- import("tpot")
 ztpot<-tpot$TPOTRegressor(generations=generationcount, population_size=retainpopulation,
                    offspring_size=offspring_size,early_stop=early_stop,max_eval_time_mins=mins_onapipe,
                    periodic_checkpoint_folder=checkpoint_folder,verbosity=2)
 
-data<-r_to_py(data)
-outcome<-r_to_py(outcome)
-ztpot$fit(data, outcome)
-prde<-ztpot$predict(data)
-RMSE(prde,py_to_r(outcome))
-SE <- RMSE(trainpred,y_train)
-predicttt <- RMSE(predictions,y_test)
+X_train<-r_to_py(X_train)
+Y_train<-r_to_py(Y_train)
+ztpot$fit(X_train, Y_train)
+prde<-ztpot$predict(X_train)
+SE <- RMSE(prde,py_to_r(Y_train))
+
+X_holdout<-r_to_py(X_holdout)
+Y_holdout<-r_to_py(Y_holdout)
+predictions<-ztpot$predict(X_holdout)
+
+predicttt <- RMSE(predictions,py_to_r(Y_holdout))
 print(predicttt)
 print(oveRMSE)
 getwd()
---
+++ pytptall.R
@@ -0,0 +1,68 @@
+#http://proceedings.mlr.press/v64/olson_tpot_2016.pdf
+
+setwd(cpout.folder)
+for(itr in c(1)){
+  fail.try=T
+  
+  try({
+when <- proc.time()
+library(reticulate)
+
+allmodel<-"TPOT"#paste("h2oAutoml",as.character(maxrun),sep = " ")
+write.table(allmodel,file = "last algorithm tried.csv",  quote = F, row.names = F,col.names = F)
+write.table(gens.names[gend.data],file = "last task tried.csv",  quote = F, row.names = F,col.names = F)
+
+if(T){
+  generationcount = r_to_py(as.integer(40))
+  retainpopulation = r_to_py(as.integer(50))
+  offspring_size = r_to_py(as.integer(100))
+  cv = r_to_py(as.integer(20))
+  random_state = r_to_py(as.integer(222))
+  early_stop = r_to_py(as.integer(3))
+  mins_onapipe = r_to_py(as.integer(30))
+  checkpoint_folder = r_to_py("tpot")
+  pipefile = r_to_py(paste("tpot","pipe",".py",sep = ""))
+}
+
+
+if(!CrashNRep(allmodel)) {
+
+tpot <- import("tpot")
+ztpot<-tpot$TPOTRegressor(generations=generationcount, population_size=retainpopulation,
+                          offspring_size=offspring_size,early_stop=early_stop,max_eval_time_mins=mins_onapipe,
+                          periodic_checkpoint_folder=checkpoint_folder,verbosity=2)
+
+
+X_train<-r_to_py(training[,-1])
+Y_train<-r_to_py(training[,1])
+ztpot$fit(X_train, Y_train)
+prde<-ztpot$predict(X_train)
+oveRMSE <- RMSE(prde,py_to_r(Y_train))
+print(oveRMSE)
+
+X_holdout<-r_to_py(testing[,-1])
+Y_holdout<-r_to_py(testing[,1])
+predictions<-ztpot$predict(X_holdout)
+predicttt <- RMSE(predictions,py_to_r(Y_holdout))
+print(predicttt)
+
+
+ztpot$export(pipefile)
+
+library(stringr)
+gDat <- read.delim(file = py_to_r(pipefile),stringsAsFactors = F)
+start<-grep("exported.*make_p", (gDat[,1]), value = F)
+end<-grep("^)$", (gDat[,1]), value = F)
+
+hyparams <- vector(mode = "character")
+hyparams <- gDat[(start+1):(end-1),1]
+hyparams <- gsub("StackingEstimator.estimator=", "Stacking", hyparams)
+hyparams <- gsub("\\(.*$", "", hyparams)
+hyparams <- gsub("[ ]+", "", hyparams)
+
+printPredMets(predicted.outcomes=predictions,overRMSE=oveRMSE,hypercount="full",libpack="tpot")
+
+    }
+  })
+}
+ 
