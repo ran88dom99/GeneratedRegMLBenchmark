2bcdc607fe48a87861e3f20a75adffe6a278a406
Ma Shin <vacation57connect@gmail.com>
2018-07-03 12:28:47 -0400

    auto

--- installTPTautoskl.sh
+++ installTPTautoskl.sh
@@ -1,7 +1,9 @@
  #!/bin/bash
 
 python -m pip install --upgrade pip
- 
+
+conda install numpy scipy scikit-learn pandas
+
 pip install deap update_checker tqdm stopit
 pip install xgboost
 pip install scikit-mdr skrebate
@@ -10,3 +12,10 @@ pip install tpot
 pip install auto_ml
 pip install stacked_generalization
 pip install mlens
+
+cd C:/xgboost
+git clone --recursive https://github.com/dmlc/xgboost 
+cd python-package
+
+ python setup.py develop --user
+ python setup.py install
