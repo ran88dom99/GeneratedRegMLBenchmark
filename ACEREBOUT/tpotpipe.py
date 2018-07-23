e1a4d5e3e99aab5457641621fb12cae1c3aecc46
ran88dom99 <rain8dome9@gmail.com>
2018-07-23 10:33:44 -0400

    long tpot

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,9 +1,12 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.feature_selection import SelectPercentile, f_regression
+from sklearn.linear_model import ElasticNetCV
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import make_pipeline, make_union
 from sklearn.preprocessing import Normalizer
+from tpot.builtins import StackingEstimator
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -11,10 +14,13 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.6971531556232848
+# Score on the training set was:-0.6424115158719266
 exported_pipeline = make_pipeline(
-    Normalizer(norm="l2"),
-    GradientBoostingRegressor(alpha=0.85, learning_rate=0.1, loss="ls", max_depth=5, max_features=0.25, min_samples_leaf=10, min_samples_split=6, n_estimators=100, subsample=0.6500000000000001)
+    Normalizer(norm="l1"),
+    StackingEstimator(estimator=ExtraTreesRegressor(bootstrap=False, max_features=0.35000000000000003, min_samples_leaf=19, min_samples_split=11, n_estimators=100)),
+    StackingEstimator(estimator=ElasticNetCV(l1_ratio=0.35000000000000003, tol=1e-05)),
+    SelectPercentile(score_func=f_regression, percentile=79),
+    ElasticNetCV(l1_ratio=0.35000000000000003, tol=0.001)
 )
 
 exported_pipeline.fit(training_features, training_target)
