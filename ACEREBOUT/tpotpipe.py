4cc1bc326ff2a3fbbf118414ad7d7e8056129003
ran88dom99 <rain8dome9@gmail.com>
2018-08-04 13:45:51 -0400

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,17 +1,12 @@
 import numpy as np
 import pandas as pd
 from sklearn.decomposition import FastICA
-from sklearn.feature_selection import SelectPercentile, VarianceThreshold, f_regression
+from sklearn.feature_selection import SelectPercentile, f_regression
 from sklearn.linear_model import LassoLarsCV
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.pipeline import make_pipeline, make_union
-from sklearn.preprocessing import Normalizer
-from sklearn.svm import LinearSVR
-from tpot.builtins import StackingEstimator, ZeroCount
-from xgboost import XGBRegressor
-from sklearn.preprocessing import FunctionTransformer
-from copy import copy
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import Normalizer, RobustScaler
+from tpot.builtins import ZeroCount
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -19,27 +14,16 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.5810950598438567
+# Score on the training set was:-0.6959507761426472
 exported_pipeline = make_pipeline(
-    make_union(
-        make_pipeline(
-            Normalizer(norm="max"),
-            SelectPercentile(score_func=f_regression, percentile=55),
-            StackingEstimator(estimator=KNeighborsRegressor(n_neighbors=26, p=2, weights="distance")),
-            Normalizer(norm="max"),
-            SelectPercentile(score_func=f_regression, percentile=38),
-            Normalizer(norm="l1"),
-            FastICA(tol=0.15000000000000002),
-            StackingEstimator(estimator=XGBRegressor(learning_rate=0.001, max_depth=5, min_child_weight=13, n_estimators=100, nthread=1, subsample=0.7500000000000001)),
-            StackingEstimator(estimator=LassoLarsCV(normalize=False)),
-            VarianceThreshold(threshold=0.01),
-            ZeroCount()
-        ),
-        FunctionTransformer(copy)
-    ),
-    SelectPercentile(score_func=f_regression, percentile=59),
-    StackingEstimator(estimator=XGBRegressor(learning_rate=0.001, max_depth=5, min_child_weight=7, n_estimators=100, nthread=1, subsample=0.45)),
-    LinearSVR(C=15.0, dual=True, epsilon=1.0, loss="epsilon_insensitive", tol=1e-05)
+    Normalizer(norm="l1"),
+    Normalizer(norm="l1"),
+    SelectPercentile(score_func=f_regression, percentile=26),
+    ZeroCount(),
+    FastICA(tol=0.25),
+    RobustScaler(),
+    ZeroCount(),
+    LassoLarsCV(normalize=True)
 )
 
 exported_pipeline.fit(training_features, training_target)
