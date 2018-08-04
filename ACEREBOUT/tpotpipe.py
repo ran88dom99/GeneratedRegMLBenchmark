9957444b12d613be3fcac93073e7dcdb5ed89f7d
ran88dom99 <rain8dome9@gmail.com>
2018-08-04 15:45:49 -0400

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,12 +1,10 @@
 import numpy as np
 import pandas as pd
-from sklearn.decomposition import FastICA
 from sklearn.feature_selection import SelectPercentile, f_regression
 from sklearn.linear_model import LassoLarsCV
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import Normalizer, RobustScaler
-from tpot.builtins import ZeroCount
+from sklearn.preprocessing import Normalizer
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -14,16 +12,11 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.6959507761426472
+# Score on the training set was:-0.845933922625692
 exported_pipeline = make_pipeline(
-    Normalizer(norm="l1"),
-    Normalizer(norm="l1"),
-    SelectPercentile(score_func=f_regression, percentile=26),
-    ZeroCount(),
-    FastICA(tol=0.25),
-    RobustScaler(),
-    ZeroCount(),
-    LassoLarsCV(normalize=True)
+    Normalizer(norm="max"),
+    SelectPercentile(score_func=f_regression, percentile=53),
+    LassoLarsCV(normalize=False)
 )
 
 exported_pipeline.fit(training_features, training_target)
