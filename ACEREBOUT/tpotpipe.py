7ef1f6930fffe5e23b8963286d97ed4807b352d6
ran88dom99 <rain8dome9@gmail.com>
2018-08-18 13:46:01 -0400

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.feature_selection import SelectPercentile, f_regression
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,8 +11,11 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.8116745810250079
-exported_pipeline = RandomForestRegressor(bootstrap=False, max_features=0.4, min_samples_leaf=12, min_samples_split=3, n_estimators=100)
+# Score on the training set was:-0.8014998794765841
+exported_pipeline = make_pipeline(
+    SelectPercentile(score_func=f_regression, percentile=68),
+    RandomForestRegressor(bootstrap=False, max_features=0.35000000000000003, min_samples_leaf=1, min_samples_split=3, n_estimators=100)
+)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
