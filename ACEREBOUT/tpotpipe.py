442be7a0a1f86a218079209d4f853417aa05086e
ran88dom99 <rain8dome9@gmail.com>
2018-09-20 13:36:26 -0400

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,12 +1,9 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import ElasticNetCV
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline, make_union
-from tpot.builtins import StackingEstimator
-from sklearn.preprocessing import FunctionTransformer
-from copy import copy
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import Normalizer
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -14,14 +11,10 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.8992166223710101
+# Score on the training set was:-0.821681703551337
 exported_pipeline = make_pipeline(
-    make_union(
-        FunctionTransformer(copy),
-        FunctionTransformer(copy)
-    ),
-    StackingEstimator(estimator=ElasticNetCV(l1_ratio=0.6000000000000001, tol=0.01)),
-    RandomForestRegressor(bootstrap=True, max_features=0.15000000000000002, min_samples_leaf=1, min_samples_split=3, n_estimators=100)
+    Normalizer(norm="l2"),
+    ElasticNetCV(l1_ratio=0.65, tol=0.001)
 )
 
 exported_pipeline.fit(training_features, training_target)
