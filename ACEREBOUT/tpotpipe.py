bf150236900ae450fd5d6fce3b3877b109124162
ran88dom99 <rain8dome9@gmail.com>
2018-10-02 13:45:59 -0400

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from sklearn.feature_selection import SelectPercentile, f_regression
 from sklearn.linear_model import ElasticNetCV
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
@@ -11,10 +12,12 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.796199317510557
+# Score on the training set was:-0.6930258399448141
 exported_pipeline = make_pipeline(
-    Normalizer(norm="l2"),
-    ElasticNetCV(l1_ratio=0.65, tol=0.001)
+    Normalizer(norm="l1"),
+    SelectPercentile(score_func=f_regression, percentile=77),
+    SelectPercentile(score_func=f_regression, percentile=29),
+    ElasticNetCV(l1_ratio=0.5, tol=0.001)
 )
 
 exported_pipeline.fit(training_features, training_target)
