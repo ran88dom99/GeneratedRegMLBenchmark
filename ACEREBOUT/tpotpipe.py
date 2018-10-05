5d4aaa3371195d5701a5fcf8f5313dfc6bd56d97
ran88dom99 <rain8dome9@gmail.com>
2018-10-05 17:45:50 -0400

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,10 +1,9 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.feature_selection import SelectPercentile, VarianceThreshold, f_regression
+from sklearn.linear_model import RidgeCV
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline, make_union
-from sklearn.svm import LinearSVR
-from tpot.builtins import StackingEstimator
+from sklearn.pipeline import make_pipeline
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -12,10 +11,11 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.8745474549668986
+# Score on the training set was:-0.8141381540246523
 exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=LinearSVR(C=15.0, dual=False, epsilon=0.1, loss="squared_epsilon_insensitive", tol=0.01)),
-    RandomForestRegressor(bootstrap=False, max_features=0.4, min_samples_leaf=14, min_samples_split=17, n_estimators=100)
+    VarianceThreshold(threshold=0.001),
+    SelectPercentile(score_func=f_regression, percentile=79),
+    RidgeCV()
 )
 
 exported_pipeline.fit(training_features, training_target)
