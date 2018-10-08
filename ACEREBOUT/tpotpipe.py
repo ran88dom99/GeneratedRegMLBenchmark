43ae9517302d380565c726298ab2450b5584bf13
ran88dom99 <rain8dome9@gmail.com>
2018-10-08 13:45:57 -0400

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,9 +1,14 @@
 import numpy as np
 import pandas as pd
 from sklearn.feature_selection import SelectPercentile, f_regression
-from sklearn.linear_model import RidgeCV
+from sklearn.linear_model import LassoLarsCV
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import MaxAbsScaler, Normalizer
+from sklearn.svm import LinearSVR
+from tpot.builtins import StackingEstimator
+from sklearn.preprocessing import FunctionTransformer
+from copy import copy
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -11,10 +16,17 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.8939103558324921
+# Score on the training set was:-0.7440020899640615
 exported_pipeline = make_pipeline(
-    SelectPercentile(score_func=f_regression, percentile=53),
-    RidgeCV()
+    make_union(
+        FunctionTransformer(copy),
+        FunctionTransformer(copy)
+    ),
+    Normalizer(norm="l2"),
+    SelectPercentile(score_func=f_regression, percentile=31),
+    MaxAbsScaler(),
+    StackingEstimator(estimator=LinearSVR(C=0.0001, dual=True, epsilon=0.001, loss="squared_epsilon_insensitive", tol=0.001)),
+    LassoLarsCV(normalize=False)
 )
 
 exported_pipeline.fit(training_features, training_target)
