af5fbd1ba5fe741442eed46e50e8d5eab333c17b
ran88dom99 <rain8dome9@gmail.com>
2018-10-11 13:45:53 -0400

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,14 +1,9 @@
 import numpy as np
 import pandas as pd
-from sklearn.feature_selection import SelectPercentile, f_regression
-from sklearn.linear_model import LassoLarsCV
+from sklearn.linear_model import ElasticNetCV
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline, make_union
-from sklearn.preprocessing import MaxAbsScaler, Normalizer
-from sklearn.svm import LinearSVR
-from tpot.builtins import StackingEstimator
-from sklearn.preprocessing import FunctionTransformer
-from copy import copy
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import Normalizer
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -16,17 +11,10 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.7440020899640615
+# Score on the training set was:-0.8108324027321248
 exported_pipeline = make_pipeline(
-    make_union(
-        FunctionTransformer(copy),
-        FunctionTransformer(copy)
-    ),
     Normalizer(norm="l2"),
-    SelectPercentile(score_func=f_regression, percentile=31),
-    MaxAbsScaler(),
-    StackingEstimator(estimator=LinearSVR(C=0.0001, dual=True, epsilon=0.001, loss="squared_epsilon_insensitive", tol=0.001)),
-    LassoLarsCV(normalize=False)
+    ElasticNetCV(l1_ratio=0.75, tol=1e-05)
 )
 
 exported_pipeline.fit(training_features, training_target)
