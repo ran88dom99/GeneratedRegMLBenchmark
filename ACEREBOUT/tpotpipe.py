e06e3bddb1056f491762d1cde001b9b54d3e954d
ran88dom99 <rain8dome9@gmail.com>
2018-10-11 19:45:49 -0400

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
-from sklearn.feature_selection import VarianceThreshold
+from sklearn.kernel_approximation import RBFSampler
 from sklearn.linear_model import RidgeCV
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
+from tpot.builtins import ZeroCount
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -11,9 +12,10 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.7916205674180872
+# Score on the training set was:-0.7880738701051562
 exported_pipeline = make_pipeline(
-    VarianceThreshold(threshold=0.001),
+    ZeroCount(),
+    RBFSampler(gamma=0.5),
     RidgeCV()
 )
 
