d769706fa49bd57a0b9cb750e504403228ff8760
ran88dom99 <rain8dome9@gmail.com>
2018-10-11 17:45:52 -0400

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,12 +1,9 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
-from sklearn.linear_model import ElasticNetCV, LassoLarsCV
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.linear_model import RidgeCV
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline, make_union
-from tpot.builtins import StackingEstimator
-from sklearn.preprocessing import FunctionTransformer
-from copy import copy
+from sklearn.pipeline import make_pipeline
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -14,18 +11,10 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.8262837827734171
+# Score on the training set was:-0.7916205674180872
 exported_pipeline = make_pipeline(
-    make_union(
-        StackingEstimator(estimator=make_pipeline(
-            StackingEstimator(estimator=ElasticNetCV(l1_ratio=0.6000000000000001, tol=0.01)),
-            GradientBoostingRegressor(alpha=0.8, learning_rate=0.001, loss="lad", max_depth=10, max_features=0.3, min_samples_leaf=14, min_samples_split=19, n_estimators=100, subsample=0.6500000000000001)
-        )),
-        FunctionTransformer(copy)
-    ),
-    StackingEstimator(estimator=GradientBoostingRegressor(alpha=0.99, learning_rate=0.01, loss="huber", max_depth=3, max_features=0.35000000000000003, min_samples_leaf=20, min_samples_split=4, n_estimators=100, subsample=0.05)),
-    StackingEstimator(estimator=LassoLarsCV(normalize=True)),
-    RandomForestRegressor(bootstrap=True, max_features=0.15000000000000002, min_samples_leaf=1, min_samples_split=3, n_estimators=100)
+    VarianceThreshold(threshold=0.001),
+    RidgeCV()
 )
 
 exported_pipeline.fit(training_features, training_target)
