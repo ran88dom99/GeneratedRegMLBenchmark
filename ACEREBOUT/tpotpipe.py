fec0aeedd188b1cba042ccfad2712d0104cc99da
ran88dom99 <rain8dome9@gmail.com>
2018-10-17 13:45:59 -0400

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,14 +1,9 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.feature_selection import SelectPercentile, f_regression
-from sklearn.linear_model import ElasticNetCV
+from sklearn.linear_model import RidgeCV
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline, make_union
-from sklearn.preprocessing import Normalizer, RobustScaler
-from sklearn.svm import LinearSVR
-from tpot.builtins import StackingEstimator
-from xgboost import XGBRegressor
+from sklearn.pipeline import make_pipeline
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -16,15 +11,10 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.6585983744362812
+# Score on the training set was:-0.8152819170358553
 exported_pipeline = make_pipeline(
-    Normalizer(norm="l2"),
-    StackingEstimator(estimator=XGBRegressor(learning_rate=1.0, max_depth=9, min_child_weight=1, n_estimators=100, nthread=1, subsample=0.05)),
-    StackingEstimator(estimator=LinearSVR(C=0.5, dual=True, epsilon=0.1, loss="squared_epsilon_insensitive", tol=0.001)),
-    SelectPercentile(score_func=f_regression, percentile=88),
-    RobustScaler(),
-    StackingEstimator(estimator=ExtraTreesRegressor(bootstrap=True, max_features=0.25, min_samples_leaf=19, min_samples_split=13, n_estimators=100)),
-    ElasticNetCV(l1_ratio=0.8, tol=0.001)
+    SelectPercentile(score_func=f_regression, percentile=53),
+    RidgeCV()
 )
 
 exported_pipeline.fit(training_features, training_target)
