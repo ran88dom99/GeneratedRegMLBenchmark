32886bf3ca2591d29f866e36e94062c5dd3a4ca3
ran88dom99 <rain8dome9@gmail.com>
2018-10-17 19:45:49 -0400

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,8 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.feature_selection import SelectPercentile, f_regression
-from sklearn.kernel_approximation import RBFSampler
-from sklearn.linear_model import ElasticNetCV
+from sklearn.linear_model import LassoLarsCV
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import Normalizer
@@ -13,14 +12,11 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.7058615070423266
+# Score on the training set was:-0.673703490186062
 exported_pipeline = make_pipeline(
     Normalizer(norm="l2"),
-    SelectPercentile(score_func=f_regression, percentile=57),
     SelectPercentile(score_func=f_regression, percentile=39),
-    RBFSampler(gamma=0.6000000000000001),
-    SelectPercentile(score_func=f_regression, percentile=39),
-    ElasticNetCV(l1_ratio=0.8, tol=0.1)
+    LassoLarsCV(normalize=False)
 )
 
 exported_pipeline.fit(training_features, training_target)
