e3a977c52058ac5a21e1b9212e3fa56926cd97f7
ran88dom99 <rain8dome9@gmail.com>
2018-10-17 17:45:49 -0400

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
 from sklearn.feature_selection import SelectPercentile, f_regression
-from sklearn.linear_model import RidgeCV
+from sklearn.kernel_approximation import RBFSampler
+from sklearn.linear_model import ElasticNetCV
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import Normalizer
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -11,10 +13,14 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.8152819170358553
+# Score on the training set was:-0.7058615070423266
 exported_pipeline = make_pipeline(
-    SelectPercentile(score_func=f_regression, percentile=53),
-    RidgeCV()
+    Normalizer(norm="l2"),
+    SelectPercentile(score_func=f_regression, percentile=57),
+    SelectPercentile(score_func=f_regression, percentile=39),
+    RBFSampler(gamma=0.6000000000000001),
+    SelectPercentile(score_func=f_regression, percentile=39),
+    ElasticNetCV(l1_ratio=0.8, tol=0.1)
 )
 
 exported_pipeline.fit(training_features, training_target)
