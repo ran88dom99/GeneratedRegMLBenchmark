0c36eb96d60e593c8f273a90c470de67a2a4ac99
ran88dom99 <rain8dome9@gmail.com>
2018-10-17 21:45:49 -0400

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pandas as pd
-from sklearn.feature_selection import SelectPercentile, f_regression
 from sklearn.linear_model import LassoLarsCV
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import Normalizer
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import Normalizer, RobustScaler
+from tpot.builtins import StackingEstimator
+from sklearn.preprocessing import FunctionTransformer
+from copy import copy
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -12,10 +14,14 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.673703490186062
+# Score on the training set was:-0.744380855244531
 exported_pipeline = make_pipeline(
+    make_union(
+        FunctionTransformer(copy),
+        FunctionTransformer(copy)
+    ),
     Normalizer(norm="l2"),
-    SelectPercentile(score_func=f_regression, percentile=39),
+    RobustScaler(),
     LassoLarsCV(normalize=False)
 )
 
