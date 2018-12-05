b3b24d647de30ce0381ae837ef38217eb253c3e3
ran88dom99 <rain8dome9@gmail.com>
2018-12-05 16:04:05 -0500

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
-from sklearn.feature_selection import SelectPercentile, f_regression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import RidgeCV
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import MaxAbsScaler
+from sklearn.svm import LinearSVR
 from tpot.builtins import StackingEstimator
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
@@ -12,11 +14,15 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.6709974510892307
+# Score on the training set was:-0.5532507922479704
 exported_pipeline = make_pipeline(
-    SelectPercentile(score_func=f_regression, percentile=61),
-    StackingEstimator(estimator=GradientBoostingRegressor(alpha=0.85, learning_rate=0.01, loss="huber", max_depth=6, max_features=1.0, min_samples_leaf=6, min_samples_split=18, n_estimators=100, subsample=0.8500000000000001)),
-    RandomForestRegressor(bootstrap=False, max_features=0.3, min_samples_leaf=11, min_samples_split=6, n_estimators=100)
+    MaxAbsScaler(),
+    StackingEstimator(estimator=RandomForestRegressor(bootstrap=False, max_features=0.4, min_samples_leaf=15, min_samples_split=9, n_estimators=100)),
+    StackingEstimator(estimator=RandomForestRegressor(bootstrap=False, max_features=0.4, min_samples_leaf=15, min_samples_split=9, n_estimators=100)),
+    StackingEstimator(estimator=LinearSVR(C=0.5, dual=False, epsilon=0.001, loss="squared_epsilon_insensitive", tol=0.01)),
+    StackingEstimator(estimator=RidgeCV()),
+    StackingEstimator(estimator=RandomForestRegressor(bootstrap=False, max_features=0.8, min_samples_leaf=6, min_samples_split=9, n_estimators=100)),
+    LinearSVR(C=0.5, dual=True, epsilon=0.001, loss="epsilon_insensitive", tol=0.0001)
 )
 
 exported_pipeline.fit(training_features, training_target)
