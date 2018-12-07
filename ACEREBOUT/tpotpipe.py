24f5c558e843637bc9710d4313db5d44d127da44
ran88dom99 <rain8dome9@gmail.com>
2018-12-07 15:45:49 -0500

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
+from sklearn.feature_selection import VarianceThreshold
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
-from sklearn.preprocessing import MinMaxScaler
-from tpot.builtins import StackingEstimator, ZeroCount
+from tpot.builtins import StackingEstimator
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -12,12 +12,11 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.6589524381210479
+# Score on the training set was:-0.802382923076923
 exported_pipeline = make_pipeline(
-    ZeroCount(),
-    MinMaxScaler(),
-    StackingEstimator(estimator=GradientBoostingRegressor(alpha=0.75, learning_rate=0.01, loss="huber", max_depth=6, max_features=0.55, min_samples_leaf=6, min_samples_split=13, n_estimators=100, subsample=0.8500000000000001)),
-    RandomForestRegressor(bootstrap=False, max_features=0.15000000000000002, min_samples_leaf=1, min_samples_split=6, n_estimators=100)
+    StackingEstimator(estimator=GradientBoostingRegressor(alpha=0.95, learning_rate=1.0, loss="lad", max_depth=6, max_features=1.0, min_samples_leaf=7, min_samples_split=7, n_estimators=100, subsample=0.6000000000000001)),
+    VarianceThreshold(threshold=0.001),
+    RandomForestRegressor(bootstrap=False, max_features=0.15000000000000002, min_samples_leaf=1, min_samples_split=3, n_estimators=100)
 )
 
 exported_pipeline.fit(training_features, training_target)
