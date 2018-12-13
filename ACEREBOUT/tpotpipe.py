e8e2842e4359e536a0cd6915718ae40826929854
ran88dom99 <rain8dome9@gmail.com>
2018-12-13 15:45:49 -0500

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,11 +1,9 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.linear_model import ElasticNetCV
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline, make_union
-from sklearn.preprocessing import MaxAbsScaler
-from tpot.builtins import StackingEstimator
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import MinMaxScaler
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -13,12 +11,10 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.6575483554933482
+# Score on the training set was:-0.836495970400837
 exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=GradientBoostingRegressor(alpha=0.85, learning_rate=0.01, loss="huber", max_depth=10, max_features=0.45, min_samples_leaf=4, min_samples_split=19, n_estimators=100, subsample=1.0)),
-    StackingEstimator(estimator=GradientBoostingRegressor(alpha=0.85, learning_rate=0.01, loss="huber", max_depth=10, max_features=0.4, min_samples_leaf=4, min_samples_split=2, n_estimators=100, subsample=1.0)),
-    MaxAbsScaler(),
-    ElasticNetCV(l1_ratio=0.55, tol=0.0001)
+    MinMaxScaler(),
+    RandomForestRegressor(bootstrap=False, max_features=0.45, min_samples_leaf=13, min_samples_split=17, n_estimators=100)
 )
 
 exported_pipeline.fit(training_features, training_target)
