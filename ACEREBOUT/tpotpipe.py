4e702fd583f363170d2bc025cd3df693c39ff180
ran88dom99 <rain8dome9@gmail.com>
2019-01-06 14:00:27 -0500

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,14 +1,12 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.linear_model import LassoLarsCV
+from sklearn.feature_selection import SelectPercentile, f_regression
+from sklearn.linear_model import RidgeCV
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
-from sklearn.preprocessing import MaxAbsScaler, RobustScaler
+from sklearn.preprocessing import Normalizer
 from sklearn.tree import DecisionTreeRegressor
-from tpot.builtins import OneHotEncoder, StackingEstimator
-from sklearn.preprocessing import FunctionTransformer
-from copy import copy
+from tpot.builtins import StackingEstimator
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -16,23 +14,15 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.698408827904663
+# Score on the training set was:-0.6729088214419051
 exported_pipeline = make_pipeline(
-    make_union(
-        StackingEstimator(estimator=DecisionTreeRegressor(max_depth=1, min_samples_leaf=6, min_samples_split=10)),
-        make_pipeline(
-            make_union(
-                FunctionTransformer(copy),
-                StackingEstimator(estimator=make_pipeline(
-                    MaxAbsScaler(),
-                    LassoLarsCV(normalize=False)
-                ))
-            ),
-            OneHotEncoder(minimum_fraction=0.1, sparse=False),
-            RobustScaler()
-        )
-    ),
-    GradientBoostingRegressor(alpha=0.9, learning_rate=0.1, loss="huber", max_depth=10, max_features=0.6000000000000001, min_samples_leaf=6, min_samples_split=10, n_estimators=100, subsample=1.0)
+    SelectPercentile(score_func=f_regression, percentile=60),
+    Normalizer(norm="l2"),
+    StackingEstimator(estimator=DecisionTreeRegressor(max_depth=8, min_samples_leaf=4, min_samples_split=15)),
+    SelectPercentile(score_func=f_regression, percentile=60),
+    Normalizer(norm="l2"),
+    Normalizer(norm="l2"),
+    RidgeCV()
 )
 
 exported_pipeline.fit(training_features, training_target)
