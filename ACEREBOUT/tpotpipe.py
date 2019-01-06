64a888d6891c93929fe30da674c9fa63b2f64a9c
ran88dom99 <rain8dome9@gmail.com>
2019-01-06 00:40:09 -0500

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,12 +1,14 @@
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.linear_model import ElasticNetCV, LassoLarsCV
+from sklearn.linear_model import LassoLarsCV
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.svm import LinearSVR
-from tpot.builtins import StackingEstimator
+from sklearn.preprocessing import MaxAbsScaler, RobustScaler
+from sklearn.tree import DecisionTreeRegressor
+from tpot.builtins import OneHotEncoder, StackingEstimator
+from sklearn.preprocessing import FunctionTransformer
+from copy import copy
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -14,14 +16,23 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.6376846004072111
+# Score on the training set was:-0.698408827904663
 exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=GradientBoostingRegressor(alpha=0.85, learning_rate=0.01, loss="huber", max_depth=6, max_features=0.45, min_samples_leaf=4, min_samples_split=2, n_estimators=100, subsample=0.9000000000000001)),
-    StackingEstimator(estimator=GradientBoostingRegressor(alpha=0.75, learning_rate=0.01, loss="huber", max_depth=6, max_features=1.0, min_samples_leaf=6, min_samples_split=13, n_estimators=100, subsample=0.8500000000000001)),
-    PolynomialFeatures(degree=2, include_bias=False, interaction_only=False),
-    StackingEstimator(estimator=ElasticNetCV(l1_ratio=0.75, tol=0.001)),
-    StackingEstimator(estimator=LassoLarsCV(normalize=True)),
-    LinearSVR(C=1.0, dual=True, epsilon=0.0001, loss="epsilon_insensitive", tol=0.001)
+    make_union(
+        StackingEstimator(estimator=DecisionTreeRegressor(max_depth=1, min_samples_leaf=6, min_samples_split=10)),
+        make_pipeline(
+            make_union(
+                FunctionTransformer(copy),
+                StackingEstimator(estimator=make_pipeline(
+                    MaxAbsScaler(),
+                    LassoLarsCV(normalize=False)
+                ))
+            ),
+            OneHotEncoder(minimum_fraction=0.1, sparse=False),
+            RobustScaler()
+        )
+    ),
+    GradientBoostingRegressor(alpha=0.9, learning_rate=0.1, loss="huber", max_depth=10, max_features=0.6000000000000001, min_samples_leaf=6, min_samples_split=10, n_estimators=100, subsample=1.0)
 )
 
 exported_pipeline.fit(training_features, training_target)
