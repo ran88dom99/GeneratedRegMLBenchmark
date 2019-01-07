866c38b4aa5ddb269a53b1f627ccaa991378862e
ran88dom99 <rain8dome9@gmail.com>
2019-01-07 00:39:05 -0500

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,12 +1,9 @@
 import numpy as np
 import pandas as pd
-from sklearn.feature_selection import SelectPercentile, f_regression
-from sklearn.linear_model import RidgeCV
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline, make_union
-from sklearn.preprocessing import Normalizer
-from sklearn.tree import DecisionTreeRegressor
-from tpot.builtins import StackingEstimator
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import RobustScaler
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -14,15 +11,10 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.6729088214419051
+# Score on the training set was:-0.8217591971544715
 exported_pipeline = make_pipeline(
-    SelectPercentile(score_func=f_regression, percentile=60),
-    Normalizer(norm="l2"),
-    StackingEstimator(estimator=DecisionTreeRegressor(max_depth=8, min_samples_leaf=4, min_samples_split=15)),
-    SelectPercentile(score_func=f_regression, percentile=60),
-    Normalizer(norm="l2"),
-    Normalizer(norm="l2"),
-    RidgeCV()
+    RobustScaler(),
+    RandomForestRegressor(bootstrap=False, max_features=0.15000000000000002, min_samples_leaf=1, min_samples_split=3, n_estimators=100)
 )
 
 exported_pipeline.fit(training_features, training_target)
