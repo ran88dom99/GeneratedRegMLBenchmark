3dfa525030dd4d1b5a2c5eb8839ee306298642af
ran88dom99 <rain8dome9@gmail.com>
2019-01-09 13:45:52 -0500

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.feature_selection import SelectPercentile, VarianceThreshold, f_regression
+from sklearn.linear_model import RidgeCV
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import MinMaxScaler, Normalizer, RobustScaler
+from tpot.builtins import ZeroCount
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -11,10 +13,16 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.8217591971544715
+# Score on the training set was:-0.7084143990953629
 exported_pipeline = make_pipeline(
+    VarianceThreshold(threshold=0.001),
+    Normalizer(norm="l2"),
     RobustScaler(),
-    RandomForestRegressor(bootstrap=False, max_features=0.15000000000000002, min_samples_leaf=1, min_samples_split=3, n_estimators=100)
+    MinMaxScaler(),
+    ZeroCount(),
+    SelectPercentile(score_func=f_regression, percentile=67),
+    SelectPercentile(score_func=f_regression, percentile=53),
+    RidgeCV()
 )
 
 exported_pipeline.fit(training_features, training_target)
