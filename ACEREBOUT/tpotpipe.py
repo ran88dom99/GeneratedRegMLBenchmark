6a202af83025ae081d824f8347b79bd076f8a5de
ran88dom99 <rain8dome9@gmail.com>
2019-02-05 22:58:07 -0500

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,11 +1,12 @@
 import numpy as np
 import pandas as pd
-from sklearn.feature_selection import SelectPercentile, VarianceThreshold, f_regression
-from sklearn.linear_model import RidgeCV
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.feature_selection import SelectPercentile, f_regression
+from sklearn.linear_model import ElasticNetCV
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import MinMaxScaler, Normalizer, RobustScaler
-from tpot.builtins import ZeroCount
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import MinMaxScaler
+from tpot.builtins import StackingEstimator
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -13,16 +14,12 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.7084143990953629
+# Score on the training set was:-0.048036536826231435
 exported_pipeline = make_pipeline(
-    VarianceThreshold(threshold=0.001),
-    Normalizer(norm="l2"),
-    RobustScaler(),
     MinMaxScaler(),
-    ZeroCount(),
-    SelectPercentile(score_func=f_regression, percentile=67),
-    SelectPercentile(score_func=f_regression, percentile=53),
-    RidgeCV()
+    SelectPercentile(score_func=f_regression, percentile=56),
+    StackingEstimator(estimator=GradientBoostingRegressor(alpha=0.8, learning_rate=0.01, loss="huber", max_depth=10, max_features=0.55, min_samples_leaf=3, min_samples_split=6, n_estimators=100, subsample=0.9000000000000001)),
+    ElasticNetCV(l1_ratio=0.15000000000000002, tol=0.001)
 )
 
 exported_pipeline.fit(training_features, training_target)
