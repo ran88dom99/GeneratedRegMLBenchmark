52fc4bf613173b4ef76669969e6428a24f52e768
ran88dom99 <rain8dome9@gmail.com>
2019-02-08 13:46:29 -0500

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.feature_selection import SelectPercentile, f_regression
-from sklearn.linear_model import ElasticNetCV
+from sklearn.feature_selection import SelectFwe, SelectPercentile, f_regression
+from sklearn.linear_model import ElasticNetCV, LassoLarsCV
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import RobustScaler
 from tpot.builtins import StackingEstimator
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
@@ -14,12 +14,14 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.048036536826231435
+# Score on the training set was:-0.03759338766230754
 exported_pipeline = make_pipeline(
-    MinMaxScaler(),
-    SelectPercentile(score_func=f_regression, percentile=56),
-    StackingEstimator(estimator=GradientBoostingRegressor(alpha=0.8, learning_rate=0.01, loss="huber", max_depth=10, max_features=0.55, min_samples_leaf=3, min_samples_split=6, n_estimators=100, subsample=0.9000000000000001)),
-    ElasticNetCV(l1_ratio=0.15000000000000002, tol=0.001)
+    StackingEstimator(estimator=GradientBoostingRegressor(alpha=0.75, learning_rate=0.01, loss="huber", max_depth=4, max_features=0.5, min_samples_leaf=2, min_samples_split=10, n_estimators=100, subsample=1.0)),
+    RobustScaler(),
+    SelectPercentile(score_func=f_regression, percentile=19),
+    StackingEstimator(estimator=ElasticNetCV(l1_ratio=0.9500000000000001, tol=0.1)),
+    SelectFwe(score_func=f_regression, alpha=0.023),
+    LassoLarsCV(normalize=True)
 )
 
 exported_pipeline.fit(training_features, training_target)
