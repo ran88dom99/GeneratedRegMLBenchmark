18d8aea81904c5320991df706b4c6789c6557ac0
ran88dom99 <rain8dome9@gmail.com>
2019-02-08 21:45:50 -0500

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,12 +1,10 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.feature_selection import SelectFwe, SelectPercentile, f_regression
-from sklearn.linear_model import ElasticNetCV, LassoLarsCV
+from sklearn.feature_selection import SelectPercentile, f_regression
+from sklearn.linear_model import LassoLarsCV
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline, make_union
-from sklearn.preprocessing import RobustScaler
-from tpot.builtins import StackingEstimator
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import Normalizer, RobustScaler
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -14,14 +12,12 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.03759338766230754
+# Score on the training set was:-0.0478556736575315
 exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=GradientBoostingRegressor(alpha=0.75, learning_rate=0.01, loss="huber", max_depth=4, max_features=0.5, min_samples_leaf=2, min_samples_split=10, n_estimators=100, subsample=1.0)),
     RobustScaler(),
-    SelectPercentile(score_func=f_regression, percentile=19),
-    StackingEstimator(estimator=ElasticNetCV(l1_ratio=0.9500000000000001, tol=0.1)),
-    SelectFwe(score_func=f_regression, alpha=0.023),
-    LassoLarsCV(normalize=True)
+    Normalizer(norm="l2"),
+    SelectPercentile(score_func=f_regression, percentile=39),
+    LassoLarsCV(normalize=False)
 )
 
 exported_pipeline.fit(training_features, training_target)
