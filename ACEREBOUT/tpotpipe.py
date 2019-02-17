b1a691a72e14a8a8557ac4876cd3de58701127eb
ran88dom99 <rain8dome9@gmail.com>
2019-02-17 13:46:21 -0500

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,11 +1,12 @@
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.feature_selection import SelectPercentile, f_regression
-from sklearn.linear_model import RidgeCV
+from sklearn.linear_model import ElasticNetCV
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
-from tpot.builtins import StackingEstimator, ZeroCount
+from tpot.builtins import StackingEstimator
+from sklearn.preprocessing import FunctionTransformer
+from copy import copy
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -13,12 +14,13 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.05082607312547181
+# Score on the training set was:-0.04549386741500877
 exported_pipeline = make_pipeline(
-    ZeroCount(),
-    SelectPercentile(score_func=f_regression, percentile=47),
-    StackingEstimator(estimator=GradientBoostingRegressor(alpha=0.75, learning_rate=0.01, loss="huber", max_depth=2, max_features=0.6000000000000001, min_samples_leaf=18, min_samples_split=11, n_estimators=100, subsample=1.0)),
-    RidgeCV()
+    make_union(
+        FunctionTransformer(copy),
+        StackingEstimator(estimator=GradientBoostingRegressor(alpha=0.75, learning_rate=0.01, loss="huber", max_depth=7, max_features=0.7000000000000001, min_samples_leaf=3, min_samples_split=13, n_estimators=100, subsample=0.8500000000000001))
+    ),
+    ElasticNetCV(l1_ratio=0.4, tol=0.001)
 )
 
 exported_pipeline.fit(training_features, training_target)
