148483661f2014044b62b2b38b2bbd823df5bcba
ran88dom99 <rain8dome9@gmail.com>
2019-02-20 13:46:19 -0500

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.linear_model import ElasticNetCV
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
-from sklearn.preprocessing import MaxAbsScaler
-from tpot.builtins import StackingEstimator
+from sklearn.preprocessing import Normalizer
+from tpot.builtins import OneHotEncoder, StackingEstimator
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -12,11 +13,12 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.052439442481161704
+# Score on the training set was:-0.04158757555836068
 exported_pipeline = make_pipeline(
-    MaxAbsScaler(),
-    StackingEstimator(estimator=GradientBoostingRegressor(alpha=0.85, learning_rate=0.1, loss="lad", max_depth=3, max_features=0.7500000000000001, min_samples_leaf=9, min_samples_split=4, n_estimators=100, subsample=0.7000000000000001)),
-    RandomForestRegressor(bootstrap=True, max_features=0.15000000000000002, min_samples_leaf=1, min_samples_split=3, n_estimators=100)
+    Normalizer(norm="max"),
+    OneHotEncoder(minimum_fraction=0.2, sparse=False),
+    StackingEstimator(estimator=GradientBoostingRegressor(alpha=0.75, learning_rate=0.01, loss="huber", max_depth=6, max_features=0.45, min_samples_leaf=6, min_samples_split=13, n_estimators=100, subsample=0.8500000000000001)),
+    ElasticNetCV(l1_ratio=0.30000000000000004, tol=0.0001)
 )
 
 exported_pipeline.fit(training_features, training_target)
