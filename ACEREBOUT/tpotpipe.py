96d7bd6f09cdc73a001d6bbc5768964989846d32
ran88dom99 <rain8dome9@gmail.com>
2019-02-26 16:35:45 -0500

    auto

--- ACEREBOUT/tpotpipe.py
+++ ACEREBOUT/tpotpipe.py
@@ -1,11 +1,9 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.linear_model import LassoLarsCV
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
-from sklearn.preprocessing import StandardScaler
-from sklearn.svm import LinearSVR
-from sklearn.tree import DecisionTreeRegressor
 from tpot.builtins import StackingEstimator
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
@@ -14,12 +12,10 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:-0.04493187245934959
+# Score on the training set was:-0.045019684860975934
 exported_pipeline = make_pipeline(
-    StandardScaler(),
-    StackingEstimator(estimator=DecisionTreeRegressor(max_depth=2, min_samples_leaf=20, min_samples_split=6)),
-    StackingEstimator(estimator=LinearSVR(C=0.0001, dual=True, epsilon=0.001, loss="squared_epsilon_insensitive", tol=0.1)),
-    RandomForestRegressor(bootstrap=False, max_features=0.25, min_samples_leaf=1, min_samples_split=3, n_estimators=100)
+    StackingEstimator(estimator=GradientBoostingRegressor(alpha=0.8, learning_rate=0.001, loss="huber", max_depth=6, max_features=0.8500000000000001, min_samples_leaf=3, min_samples_split=17, n_estimators=100, subsample=0.9000000000000001)),
+    LassoLarsCV(normalize=False)
 )
 
 exported_pipeline.fit(training_features, training_target)
