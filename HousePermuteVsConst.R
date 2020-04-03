7192db01e80689509072713b5496288fcee8fa0a
ran88dom99 <rain8dome9@gmail.com>
2020-04-03 13:46:05 -0400

    auto

--- HousePermuteVsConst.R
+++ HousePermuteVsConst.R
@@ -26,11 +26,8 @@ bad.models=c("randomGLM","DENFIS","neuralnet","partDSA","blackboost","bstSm","bs
              "qrnn","mxnet","mxnetAdam","WM")
 
 allmodels <- setdiff(allmodels,bad.models)
-if(exists("recoutr2")){
-  allmodels <- setdiff(allmodels,rownames(recoutr2))
-} else {
-  recoutr <- data.frame()
-  recoutr2 <- data.frame()
+load("~/GitHub/GeneratedRegMLBenchmark/routHouse.Rdata")
+if(!exists("justmeans")){
   justmeans <- data.frame(nams=c("ideal_corrs_w_y", 
                                  "modl_w_x",
                                  "modl_wo_x",
@@ -44,8 +41,12 @@ if(exists("recoutr2")){
                                  "wo_x_n_modl_x_quant_mean",
                                  "wo_x_n_modl_x_perm_met",
                                  "wo_x_n_modl_x_perm_met_sd"))
+  
+} 
   finished <- vector()
-}
+  recoutr <- data.frame()
+  recoutr2 <- data.frame()
+
 
 library(MASS)
 bs<-Boston
@@ -70,9 +71,9 @@ for(allmodel in allmodels){#allmodel<-allmodels[3]
     #(y~.,daata),
   }
   print(paste(allmodel,date()))
-  record <- data.frame()
+  #record <- data.frame()
   try({
-    for(i in 1:iter){#i<-1
+    for(izzyx in 1:iter){#i<-1
       ex <- sample(1:leng,leng/3) #testing partition
       rx <- setdiff(c(1:leng),c(ex))#training partition
       y <- bs$y
@@ -107,7 +108,10 @@ for(allmodel in allmodels){#allmodel<-allmodels[3]
         tb = data.frame(y[ex]) #build up for permutation mean then metric
         tb <- tb[,-1]
         tq <- vector(mode = "numeric",length = dim(f)[1])
-        for(i in quantile(d[,col],seq(.1,.9,.2))){#i<-quantile(f[,col])[1]
+        qit <- 5
+        a <- (1/qit)/2; b <- (1-(1/qit)/2); c <- (((b-a)/qit))
+        qunt <- quantile(d[,col],seq(a,b,c))
+        for(i in qunt){#i<-qunt[1]
           g <- dt
           tq[] <- i
           tq[] <- f[which.min(abs(f[,col]-tq)),col]
@@ -133,25 +137,29 @@ for(allmodel in allmodels){#allmodel<-allmodels[3]
       ylxpsd <- sd(tbx)
       })
       
-      record <- rbind(record,data.frame(1, metr(y[ex],yl),metr(y[ex],ylx),metr(y[ex],ylm),
-                                      metr(y[ex],ylp),metr(y[ex],ylq),ylpm,ylpsd,
-                                      metr(ylx,ylm),metr(ylx,ylp),metr(ylx,ylq),ylxpm,ylxpsd))
+      record1 <- data.frame(allmodel,colnam=names(bs)[col],
+                             ideal_corrs_w_y=1,
+                            modl_w_x=metr(y[ex],yl),
+                            modl_wo_x=metr(y[ex],ylx),
+                            modl_mean_x=metr(y[ex],ylm),
+                            modl_x_perm_mean=metr(y[ex],ylp),
+                            modl_x_quant_mean=metr(y[ex],ylq),
+                            modl_x_perm_met=ylpm,
+                            modl_x_perm_met_sd=ylpsd,
+                            wo_x_n_modl_mean_x=metr(ylx,ylm),
+                            wo_x_n_modl_perm_mean=metr(ylx,ylp),
+                            wo_x_n_modl_x_quant_mean=metr(ylx,ylq),
+                            wo_x_n_modl_x_perm_met=ylxpm,
+                            wo_x_n_modl_x_perm_met_sd=ylxpsd)
+      
+      if (!exists("record")) {
+        record <- record1
+      } else {
+      record <- rbind(record,record1)
+      }
+      
     }
-    
-    names(record)<-c("ideal_corrs_w_y", 
-                     "modl_w_x",
-                     "modl_wo_x",
-                     "modl_mean_x",
-                     "modl_x_perm_mean",
-                     "modl_x_quant_mean",
-                     "modl_x_perm_met",
-                     "modl_x_perm_met_sd",
-                     "wo_x_n_modl_mean_x",
-                     "wo_x_n_modl_perm_mean",
-                     "wo_x_n_modl_x_quant_mean",
-                     "wo_x_n_modl_x_perm_met",
-                     "wo_x_n_modl_x_perm_met_sd")
-    
+    save(record,file = "rouRecHouse.Rdata")
     print(record)
     if(F){
     for(i in 1:dim(record)[2]){
@@ -173,15 +181,15 @@ for(allmodel in allmodels){#allmodel<-allmodels[3]
     recoutr2<-rbind(recoutr2,out)
     save(recoutr2,file = "routHouseL2Corr.Rdata")
     }
-    
+    if(F){
     avc<-vector()
     for(i in 1:dim(record)[2]){
       avc<-c(avc,mean(record[,i],na.rm = T))
     }
     justmeans<-cbind(justmeans,data.frame(avc))
     names(justmeans)[dim(justmeans)[2]]<-paste0(allmodel,"_",names(bs)[col])
-    save(justmeans,file = "routHouse.Rdata")
-    
+      save(justmeans,file = "routHouse.Rdata")
+    }
     finished<-c(finished,allmodel)
   })
 }
@@ -195,8 +203,11 @@ recoutr;finished
 
 ##### IMPORTANT TODO #####
 #where do we get the data for permuting? Training OFC! DONE. but not in first run
-#quantiles represent edges of cuts not centers; fix this.
+# hopefully sources for X substitutes are from the training set now
+#quantiles represent edges of cuts not centers; fix this. DONE
 #mean before or after proximity metric, lets see what shows up in aggregate anylis
 #what about Standard deviation? also after anyl
-
-
+# record every time run not just mean and at that time ignore sd
+#no more mxnet no more WM
+# I cannnot concentrate enough to tell what is causing quant to create identical scores to mean
+# its the correlation; the permuttion changes order but quant does not
